1caa677025ea5aded3e19eed55d1aa5963143e7f
junjeejj <junjeejj@gmail.com>
2025-11-08 00:51:25 +0900

    adsense: wire display ad slot 1760480869 to top/bottom banners

--- src/components/BannerBottom.tsx
+++ src/components/BannerBottom.tsx
@@ -11,13 +11,9 @@ export default function BannerBottom({ pathname }: { pathname: string }) {
   return (
 
     <div className="fixed bottom-0 left-0 right-0 z-50 bg-white border-t">
-
       <div className="mx-auto max-w-6xl px-3 py-2">
-
-        <AdSlot slot="여기에_하단슬롯ID" style={{ minHeight: 60 }} />
-
+        <AdSlot slot="1760480869" style={{ minHeight: 60 }} />
       </div>
-
     </div>
 
   );
