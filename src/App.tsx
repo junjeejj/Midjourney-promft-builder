5b96f5b75b9822ecc6b4204ac15d6738d271c1a8
junjeejj <junjeejj@gmail.com>
2025-11-08 01:30:04 +0900

    revert: remove diagnostic screen and restore router/layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,139 +1,77 @@
 import React from "react";
-
 import { Routes, Route, Outlet, useLocation } from "react-router-dom";
 
-
-
 import BannerTop from "./components/BannerTop";
-
 import BannerBottom from "./components/BannerBottom";
-
 import ErrorBoundary from "./components/system/ErrorBoundary";
 
-
-
-// 실제 존재하는 페이지들만 import
-
+// 실제 페이지들 (프로젝트에 있는 페이지로 유지)
 import Builder from "./pages/Builder";
-
 import Pricing from "./pages/Pricing";
-
 import Login from "./pages/Login";
 
+declare global {
+  interface Window {
+    adsbygoogle: any[];
+  }
+}
 
-
-declare global { interface Window { adsbygoogle: any[] } }
-
-
-
-// 라우트 변경시 광고 리프레시(있어도 렌더 방해 X)
-
+// 라우트 변경 때 AdSense 리프레시 (광고 자원 재요청)
 function UseAdsOnRouteChange() {
-
   const { pathname } = useLocation();
-
   React.useEffect(() => {
-
-    try { (window.adsbygoogle = window.adsbygoogle || []).push({}); } catch {}
-
+    try {
+      (window.adsbygoogle = window.adsbygoogle || []).push({});
+    } catch {}
   }, [pathname]);
-
   return null;
-
 }
 
-
-
+// 공통 레이아웃: 상/하단 배너 + 여백
 function Layout() {
-
   const { pathname } = useLocation();
-
   return (
-
     <>
-
       <UseAdsOnRouteChange />
-
       <BannerTop pathname={pathname} />
-
       <div className="pt-14 pb-16">
-
         <Outlet />
-
       </div>
-
       <BannerBottom pathname={pathname} />
-
     </>
-
   );
-
 }
-
-
 
 export default function App() {
-
   return (
-
     <ErrorBoundary>
-
       <Routes>
-
         <Route element={<Layout />}>
-
-          {/* 홈을 Builder로 매핑 */}
-
+          {/* 홈을 빌더로 연결 */}
           <Route
-
             path="/"
-
             element={
-
               <ErrorBoundary>
-
                 <Builder />
-
               </ErrorBoundary>
-
             }
-
           />
-
           <Route
-
             path="/builder"
-
             element={
-
               <ErrorBoundary>
-
                 <Builder />
-
               </ErrorBoundary>
-
             }
-
           />
-
           <Route path="/pricing" element={<Pricing />} />
-
         </Route>
-
-
-
-        {/* 유틸 페이지(광고 비노출 가능) */}
-
+        {/* 광고 비노출 권장 라우트 */}
         <Route path="/login" element={<Login />} />
-
-        <Route path="/success" element={<div style={{padding:24}}>결제가 완료되었습니다.</div>} />
-
-        <Route path="*" element={<div style={{padding:24}}>페이지를 찾을 수 없습니다.</div>} />
-
+        <Route path="/success" element={<div style={{ padding: 24 }}>결제가 완료되었습니다.</div>} />
+        {/* 404 */}
+        <Route path="*" element={<div style={{ padding: 24 }}>페이지를 찾을 수 없습니다.</div>} />
       </Routes>
-
     </ErrorBoundary>
-
   );
-
 }
